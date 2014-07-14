0e8d0eb43cfb50791405300b4f0beb67bc75409f
colin <hitwhylz@163.com>
2014-07-14 15:26:16 +0800

    Initial Commit

---
+++ BeautyDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeautyDemo
+//
+//  Created by Colin on 14-7-14.
+//  Copyright (c) 2014年 icephone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeautyDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BeautyDemo
+//
+//  Created by Colin on 14-7-14.
+//  Copyright (c) 2014年 icephone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
